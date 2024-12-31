3a56423308ed3a6e2f895dc09b6dc53852ad4c6f
makowithyou <mako.withyou@gmail.com>
2024-12-31 14:36:03 +0900

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -3,7 +3,7 @@ const hamburger = document.getElementById("hamburger");
 hamburger.addEventListener(
     "click",
     (event) => {
-        const hamburger_lines = event.target.children;
+        const hamburger_lines = event.currentTarget.children;
 
         for (const line of hamburger_lines) {
             if (line.classList.length == 0) {
