97f9a66bbeac995f9fbb575071cde3a51a202d69
makowithyou <mako.withyou@gmail.com>
2025-01-06 13:45:39 +0900

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,7 @@
 const hamburger = document.getElementById("hamburger");
-const aside = document.getElementsByTagName("aside");
+const asides = document.getElementsByTagName("aside");
+console.log(asides);
+
 
 hamburger.addEventListener(
     "click",
@@ -7,11 +9,16 @@ hamburger.addEventListener(
         const hamburger_lines = event.currentTarget.children;
         for (const line of hamburger_lines) {
             if (line.classList.length == 0) {
-                line.className = "open";
+                line.classList.add("open");
             } else {
                 line.classList.toggle("open");
                 line.classList.toggle("close");
             }
+        }
+
+        for (const aside of asides) {
+            console.log(aside);
+            aside.classList.toggle("open");
         }
     }
 );
