c3faab9b103cbd86461a2ad2335035625ddb68e5
makowithyou <mako.withyou@gmail.com>
2025-01-06 13:27:51 +0900

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,19 +1,16 @@
 const hamburger = document.getElementById("hamburger");
+const aside = document.getElementsByTagName("aside");
 
 hamburger.addEventListener(
     "click",
     (event) => {
         const hamburger_lines = event.currentTarget.children;
-
         for (const line of hamburger_lines) {
             if (line.classList.length == 0) {
-                line.classList.add("open");
-            } else if (line.classList[0] == "open") {
-                line.classList.remove("open");
-                line.classList.add("close");
-            } else if (line.classList[0] == "close") {
-                line.classList.remove("close");
-                line.classList.add("open");
+                line.className = "open";
+            } else {
+                line.classList.toggle("open");
+                line.classList.toggle("close");
             }
         }
     }
