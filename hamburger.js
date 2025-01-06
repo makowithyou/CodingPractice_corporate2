2658d5e3615f9e18e525366a31272522b560c703
makowithyou <mako.withyou@gmail.com>
2025-01-06 11:10:31 +0900

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -8,8 +8,13 @@ hamburger.addEventListener(
         for (const line of hamburger_lines) {
             if (line.classList.length == 0) {
                 line.classList.add("clicked");
-            } else {
-                line.classList.remove("clicked");
+                line.classList.add("open");
+            } else if (line.classList[1] == "open") {
+                line.classList.remove("open");
+                line.classList.add("close");
+            } else if (line.classList[1] == "close") {
+                line.classList.remove("close");
+                line.classList.add("open");
             }
         }
     }
