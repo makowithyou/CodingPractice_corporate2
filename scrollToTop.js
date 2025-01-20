a2b8d5e67bfa3faff9346897d6569fdf78fabe80
makowithyou <mako.withyou@gmail.com>
2025-01-20 06:37:34 +0400

    Update scrollToTop.js

--- scrollToTop.js
+++ scrollToTop.js
@@ -1,5 +1,5 @@
 const ScrollButton = document.getElementById("ScrollButton");
-
+// スクロール時に「トップへ戻るボタン」を表示・非表示
 document.addEventListener("scroll", (event) => {
     if (window.scrollY > 700) {
         ScrollButton.classList.add("show");
@@ -7,7 +7,7 @@ document.addEventListener("scroll", (event) => {
         ScrollButton.classList.remove("show");
     }
 });
-
+// 「トップへ戻るボタン」クリック時に上にスクロール
 ScrollButton.addEventListener(
     "click",
     (event) => {
